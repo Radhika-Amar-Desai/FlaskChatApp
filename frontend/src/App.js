4af42742a2d0762978170ef82471278aa4ec41a3
Radhika-Amar-Desai <120047754+Radhika-Amar-Desai@users.noreply.github.com>
2024-11-17 03:31:11 +0530

    Changed API_URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "http://51.20.42.1:5000";
 
 function App() {
   const [username, setUsername] = useState("");
